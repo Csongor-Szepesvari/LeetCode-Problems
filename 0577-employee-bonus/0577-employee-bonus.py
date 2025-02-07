d170d652418fdcbf9cb412c2736a835fcf8a5bcc
Csongor Szepesvari <csongor.szepesvari@gmail.com>
2025-02-07 11:08:10 +1100

    0577-EMPLOYEE-BONUS Stats: Time: 300 ms (99.93%), Space: 68.4 MB (9.3%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.py
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -4,9 +4,9 @@ def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
     # write the name and bonus of each employee, where bonus < 1000
     matching_rows = employee.merge(bonus, on='empId', how='left')
     #print(matching_rows)
-    print(matching_rows[["name", "bonus"]])
+    #print(matching_rows[["name", "bonus"]])
     
-    output = matching_rows[(matching_rows["bonus"].isna()) | (matching_rows["bonus"] < 1000)]
+    return matching_rows[(matching_rows["bonus"].isna()) | (matching_rows["bonus"] < 1000)][["name", "bonus"]]
     #print(output)
-    return output[["name", "bonus"]]
+    #return output[["name", "bonus"]]
    
